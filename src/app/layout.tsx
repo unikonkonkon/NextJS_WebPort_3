9277d51e4bea06036c76c631525aa76c6f4b6f8e
suthep <suthep@iapp.co.th>
2025-11-04 17:52:40 +0700

    change img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Test Portfolio",
   description: "Test Portfolio Description",
+  icons: {
+    icon: "/file.svg",
+  },
 };
 
 export default function RootLayout({
